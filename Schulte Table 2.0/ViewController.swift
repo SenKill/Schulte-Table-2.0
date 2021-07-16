8f866e9d513eb9bbefda38aa4d935478661fdc4d
SenKill <serik1404500@gmail.com>
2021-07-16 13:19:13 +0600

    Initial Commit

---
+++ Schulte Table 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schulte Table 2.0
+//
+//  Created by SenKill on 7/16/21.
+//  Copyright © 2021 SenKill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
